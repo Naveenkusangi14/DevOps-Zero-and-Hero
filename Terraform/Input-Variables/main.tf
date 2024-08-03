295dc7a4af10b844f1303cade6fe02b8bf52e919
naveenkusangi14 <naveenkusangi14@gmail.com>
2024-08-03 19:23:21 +0530

    Update Main.tf file with List and Maps

--- Terraform/Input-Variables/main.tf
+++ Terraform/Input-Variables/main.tf
@@ -13,10 +13,126 @@ resource "aws_instance" "ec2_example" {
            Name = "Main"
    }
 }
-
+# String 
 variable "instance_type" {
    description = "Instance type t2.micro"
    type        = string
    default     = "t2.micro"
 }
+
+# Number
+
+provider "aws" {
+   region     = "eu-central-1"
+   access_key = "<INSERT_YOUR_ACCESS_KEY>"
+   secret_key = "<INSERT_YOUR_SECRET_KEY>"
+}
+
+resource "aws_instance" "ec2_example" {
+
+   ami           = "ami-0767046d1677be5a0"
+   instance_type =  "t2.micro"
+   count = var.instance_count
+
+   tags = {
+           Name = "Terraform EC2"
+   }
+}
+
+variable "instance_count" {
+  description = "EC2 instance count"
+  type        = number
+  default     = 2
+}
+
+# Bool varaiable for showing Public or Not"
+
+provider "aws" {
+   region     = "eu-central-1"
+   access_key = "<INSERT_YOUR_ACCESS_KEY>"
+   secret_key = "<INSERT_YOUR_SECRET_KEY>"
+}
+
+
+resource "aws_instance" "ec2_example" {
+
+   ami           = "ami-0767046d1677be5a0"
+   instance_type =  "t2.micro"
+   count = 1
+  associate_public_ip_address = var.enable_public_ip
+
+   tags = {
+           Name = "Terraform EC2"
+   }
+
+}
+
+variable "enable_public_ip" {
+  description = "Enable public IP address"
+  type        = bool
+  default     = true
+}
+
+
+# LIST TO ADD IAM USER IT WILL MAIN FOR ADMIN TO ADD MULTIPLE USERS
+
+provider "aws" {
+   region     = "eu-central-1"
+   access_key = "<INSERT_YOUR_ACCESS_KEY>"
+   secret_key = "<INSERT_YOUR_SECRET_KEY>"
+}
+resource "aws_instance" "ec2_example" {
+
+   ami           = "ami-0767046d1677be5a0"
+   instance_type =  "t2.micro"
+   count = 1
+
+   tags = {
+           Name = "Terraform EC2"
+   }
+
+}
+
+resource "aws_iam_user" "example" {
+  count = length(var.user_names)
+  name  = var.user_names[count.index]
+}
+
+variable "user_names" {
+  description = "IAM usernames"
+  type        = list(string)
+  default     = ["user1", "user2", "user3s"]
+}
+
+
+# MAP IT IS MAIN USE FOR THE MAPPING PROJECTS 
+
+provider "aws" {
+   region     = "eu-central-1"
+   access_key = "<INSERT_YOUR_ACCESS_KEY>"
+   secret_key = "<INSERT_YOUR_SECRET_KEY>"
+}
+resource "aws_instance" "ec2_example" {
+
+   ami           = "ami-0767046d1677be5a0"
+   instance_type =  "t2.micro"
+
+   tags = var.project_environment
+
+}
+
+
+variable "project_environment" {
+  description = "project name and environment"
+  type        = map(string)
+  default     = {
+    project     = "project-alpha",
+    environment = "dev"
+  }
+}
+
+
+
+
+
 
